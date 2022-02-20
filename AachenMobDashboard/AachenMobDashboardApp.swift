2821b152fb57d8f55505f118be37b435d07d7486
Christian Menschel <christian@tapwork.de>
2022-02-20 13:17:09 +0100

    Initial Commit

---
+++ AachenMobDashboard/AachenMobDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AachenMobDashboardApp.swift
+//  AachenMobDashboard
+//
+//  Created by Christian Menschel on 20.02.22.
+//
+
+import SwiftUI
+
+@main
+struct AachenMobDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
