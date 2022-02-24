b405a8614e34717e4b9d4a55c8e0498740cb9554
Christian Menschel <christian@tapwork.de>
2022-02-24 18:33:55 +0100

    Fix dependencies

--- AachenMobDashboard/Domains/ChargeStations/Package.swift
+++ AachenMobDashboard/Domains/ChargeStations/Package.swift
@@ -16,14 +16,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "API", path: "../../API")
+        .package(name: "API", path: "../../API"),
+        .package(name: "Location", path: "../../Location")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ChargeStations",
-            dependencies: ["API"]),
+            dependencies: ["API", "Location"]),
         .testTarget(
             name: "ChargeStationsTests",
             dependencies: ["ChargeStations"]),
